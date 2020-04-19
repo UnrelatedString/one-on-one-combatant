f3bad14acda061e182d80f21b7dbbbb1cfd16d3a
Kai Hoop <unrelatedstring@gmail.com>
2020-04-19 06:01:50 -0400

    prettier help text oh also challenge

--- commands.py
+++ commands.py
@@ -1,3 +1,5 @@
+import asyncio
+
 cmds = {}
 docs = {}
 helptext = ''
@@ -16,5 +18,17 @@ async def help(bot, args, message):
     '''Prints this list of commands.'''
     await message.channel.send(helptext)
 
+@cmd('duel', 'play', 'battle')
+async def challenge(bot, args, message):
+    ch = message.channel
+    timeout = 60
+    if not message.mentions:
+        await ch.send('You must mention a user to challenge them.')
+        return
+    opponent = message.mentions[0]
+    await ch.send(f'{message.author.display_name} has challenged {opponent.display_name} to a duel!\nThey have {timeout} seconds to accept.')
+    await asyncio.sleep(timeout)
+    await ch.send('Challenge timed out.')
+
 for names in sorted(docs):
-    helptext += f'**{", ".join(names)}:** {docs[names]}\n'
+    helptext += f'> **{", ".join(names)}:** {docs[names]}\n'
