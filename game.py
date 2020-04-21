9fe83016706f65413cb492a379125d790b943eb6
Kai Hoop <unrelatedstring@gmail.com>
2020-04-21 06:00:33 -0400

    RPS to determine turn order... not that I've tested it with someone else

--- game.py
+++ game.py
@@ -2,14 +2,14 @@ import random
 import asyncio
 from discord import ChannelType
 
-choices = ('Rock', 'Paper', 'Scissors')
+rps_options = ('Rock', 'Paper', 'Scissors')
 
 class Game:
 
     def __init__(self, ch, wait_for, player1, player2):
         self.ch = ch
         self.wait_for = lambda event, check: wait_for(event, check=check)
-        self.player1, self.player2 = random.sample((player1, player2), 2)
+        self.player1, self.player2 = player1, player2
 
     async def broadcast(self, text):
         await asyncio.wait((self.ch.send(text),
@@ -34,7 +34,7 @@ class Game:
 
     async def wait_for_ff(self):
         message = await self.wait_for('message', self.is_ff)
-        await self.broadcast(f"{message.author.display_name} has forfeit the match. {self.player1 if message.author == self.player2 else self.player2} wins!")
+        await self.broadcast(f"{message.author.display_name} has forfeit the match. {(self.player1 if message.author == self.player2 else self.player2).display_name} wins!")
 
     def is_ff(self, message):
         return message.channel.type == ChannelType.private and message.content == 'ff'
@@ -50,24 +50,27 @@ class Game:
 
     async def main(self):
         await self.broadcast("Rock, paper, scissors, shoot!")
-        await self.rps()
+        rps_winner = await self.rps()
+        prefers_right = await self.choose_side(rps_winner)
+        if prefers_right ^ (rps_winner == self.player2):
+            self.player1, self.player2 = self.player2, self.player1
+        await self.broadcast(f"{self.player1.display_name} went first, topdecked Exodia, and won instantly. A winner is them")
 
     async def rps(self):
         choice1, choice2 = await self.for_both(self.rps_to)
         diff = (choice1 - choice2) % 3
         if diff == 0:
-            await self.broadcast(f"Both chose **{choices[choice1]}**! A tie!")
+            await self.broadcast(f"Both chose **{rps_options[choice1]}**! A tie!")
             return await self.rps()
         elif diff == 2:
-            await self.broadcast(f"**{choices[choice2]}** beats **{choices[choice1]}**! {self.player2.display_name} wins!")
+            await self.broadcast(f"**{rps_options[choice2]}** beats **{rps_options[choice1]}**! {self.player2.display_name} wins!")
             return self.player2
         else:
-            await self.broadcast(f"**{choices[choice1]}** beats **{choices[choice2]}**! {self.player1.display_name} wins!")
+            await self.broadcast(f"**{rps_options[choice1]}** beats **{rps_options[choice2]}**! {self.player1.display_name} wins!")
             return self.player1
 
     async def rps_to(self, player):
         ch = player.dm_channel
-        msg = None
         async with ch.typing():
             msg = await ch.send("React to choose rock, paper, or scissors")
             print(msg.id)
@@ -84,5 +87,22 @@ class Game:
         reaction, _ = await getreaction
         print(f'Got reaction to {msg.id}')
         choice = ord(str(reaction.emoji)) - 0x270a
-        await ch.send(f"You chose **{choices[choice]}**.")
+        await ch.send(f"You chose **{rps_options[choice]}**.")
+        return choice
+
+    async def choose_side(self, player):
+        ch = player.dm_channel
+        async with ch.typing():
+            msg = await ch.send("Do you want first turn on the left, or second turn on the right?")
+            await msg.add_reaction('\U0001f448')
+            await msg.add_reaction('\U0001f449')
+            await asyncio.sleep(1)
+        getreaction = self.wait_for('reaction_add',
+                                    lambda reaction, user:
+                                        print(reaction.message.id, msg.id) or
+                                        reaction.message.id == msg.id and
+                                        str(reaction.emoji) in '\U0001f448\U0001f449')
+        reaction, _ = await getreaction
+        choice = ord(str(reaction.emoji)) - 0x1f448
+        await self.broadcast(f"{player.display_name} has chosen the {('left', 'right')[choice]} side.")
         return choice
